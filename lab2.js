289fea5f932b03010db8a5f5bf69f44c059787ae
Merijn <99054535@mydavinci.nl>
2019-09-11 13:20:46 +0200

    Lab 2
    Ik ben slecht hier in help

---
+++ lab2.js
@@ -0,0 +1,4 @@
+var firstname = prompt("Graag uw voornaam invoeren");
+var surname = prompt("Graag uw achternaam invoeren");
+var age = prompt("Graag uw leeftijd aangeven");
+document.write(firstname +" "+ surname +"<p> Da vinci");
